c96420aa21a972c3b50442de9db7d8a976cdb097
Meredith Erb <mereditherb@merediths-mbp.attlocal.net>
2017-01-03 22:29:40 -0500

    added CSS to functioning gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,6 @@
-//look at movies-button-layout for html assistance of layout maybe(will help with classes/ids)
 $(document).ready(function() {
 
 var animals = ["cat", "bird", "rat", "dog", "goat", "donkey", "chicken", "pig"];
-// var APIKey = "dc6zaTOxFJmzC";
 
 $(document).on("click", ".animal", function() {
   $("#animals-here").empty();  
@@ -10,7 +8,7 @@ $(document).on("click", ".animal", function() {
         var animalKind = $(this).attr("data-value");
         console.log(this);
 
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalKind + "&api_key=dc6zaTOxFJmzC&limit=10&rating=";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalKind + "&api_key=dc6zaTOxFJmzC&limit=10&rating=";
 
         $.ajax({
           url: queryURL,
@@ -28,22 +26,21 @@ $(document).on("click", ".animal", function() {
               var p = $("<p>").text("Rating: " + animalRating);
               var gif = $("<img>");
 
-              var motionGif = animalResult[i].images.original.url;//testing out to see if works as a variable
-              var stillGif = animalResult[i].images.original_still.url;
+              var motionGif = animalResult[i].images.fixed_height.url;
+              var stillGif = animalResult[i].images.fixed_height_still.url;
 
               gif.addClass("gifMotion");
-              gif.attr("src", animalResult[i].images.original_still.url);
-              //this above works but seeing if variable will work
+              gif.attr("src", animalResult[i].images.fixed_height_still.url);
               gif.attr("data-still", stillGif);
               gif.attr("data-animate", motionGif);
               gif.attr("data-value", "still");
-              // gif.attr("src", animalResult[i].images.original.url);
-              //attr for motion gif
+              
               animalDiv.append(gif);
               animalDiv.append(p);
               $("#animals-here").append(animalDiv);
-
-              
+              // $("#animals-here").append(gif); 
+              // $("#animals-here").append(p); 
+ 
             }
 
           }
@@ -51,25 +48,19 @@ $(document).on("click", ".animal", function() {
               var state = $(this).attr("data-value");
               console.log(this);
                 if (state === "still") {
-                    // gif.attr("src", animalResult[i].images.original.url);
-                    // gif.attr("src", motionGif);
                     $(this).attr("src", $(this).data("animate"));
-                    // $(this).attr("src", motionGif);
-                    // gif.attr("data-value", "motion");
                     $(this).attr("data-value", "animate");
-                    animalDiv.append(gif);
-                    $("#animals-here").append(gif);
-                }
-                else if (state === "animate")  {
+                    // animalDiv.append(gif);
+                    // $("#animals-here").append(gif);
+                } else if (state === "animate")  {
                     $(this).attr("src", $(this).data("still"));
                     $(this).attr("data-value", "still");
-                    animalDiv.append(gif);
-                    $("#animals-here").append(gif);
+                    // animalDiv.append(gif);
+                    // $("#animals-here").append(gif);
 
                 }
 
               });
-          // $("#animalsGif").html(JSON.stringify(response));
           renderButtons();
         });
       
@@ -96,10 +87,6 @@ $("#add-animal").on("click", function(event) {
 
   renderButtons();
 });
-
-  
-
-
-    renderButtons();
+  renderButtons();
 
 });
